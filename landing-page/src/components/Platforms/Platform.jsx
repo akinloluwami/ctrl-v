8c8db107a2b9eec38abe7a42c6330eb46119896a
Timileyin <timileyinoyelekan11@gmail.com>
2009-01-06 01:28:54 +0100

    feat: redesign platform section

--- landing-page/src/components/Platforms/Platform.jsx
+++ landing-page/src/components/Platforms/Platform.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Platform({ name, comingSoon, logo }) {
   return (
     <div className="platform" id="platforms">
-      {comingSoon && <p>Coming soon</p>}
-      {name === "Blackberry" && <p>Just kidding lol</p>}
+      {comingSoon && <p className="coming-soon">Coming soon</p>}
+      {/* {name === "Blackberry" && <p>Just kidding lol</p>} */}
       <div className="platform__logo">{logo}</div>
       <h3>{name}</h3>
     </div>
--- landing-page/src/components/Platforms/Platforms.jsx
+++ landing-page/src/components/Platforms/Platforms.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import logo from "../../assets/ctrlV_logo.png";
 import { IoLogoAndroid } from "react-icons/io";
 import { BsApple, BsWindows } from "react-icons/bs";
 import { VscTerminalLinux } from "react-icons/vsc";
@@ -71,18 +73,37 @@ function Platforms() {
       logo: <SiFigma />,
     },
   ];
+  useEffect(() => {
+    document.querySelectorAll(".circular > *").forEach((el, i) => {
+      //  var letter = text[i];
+      // var span = document.createElement('span');
+      // var node = document.createTextNode(el);
+      var r = (360 / 11) * i;
+      var x = (Math.PI / 11).toFixed(0) * i;
+      var y = (Math.PI / 11).toFixed(0) * i;
+      // span.appendChild(node);
+      el.style.webkitTransform =
+        "rotateZ(" + r + "deg) translate3d(" + x + "px," + y + "px,0)";
+      el.style.transform =
+        "rotateZ(" + r + "deg) translate3d(" + x + "px," + y + "px,0)";
+      // element.appendChild(span);
+    });
+  }, []);
   return (
     <>
       <h1 className="h1__text">Supported Platforms</h1>
       <div className="platforms">
-        {platforms.map((platform) => (
-          <Platform
-            key={platforms.indexOf(platform)}
-            name={platform.name}
-            comingSoon={platform.comingSoon}
-            logo={platform.logo}
-          />
-        ))}
+        <img src={logo} className="logo" />
+        <div className="circular">
+          {platforms.map((platform) => (
+            <Platform
+              key={platforms.indexOf(platform)}
+              name={platform.name}
+              comingSoon={platform.comingSoon}
+              logo={platform.logo}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
