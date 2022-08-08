fb44453293e58a2ffff790689bc814dadf0e64e9
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-08 01:16:13 +0100

    set up user schema

---
+++ backend/src/schema/User.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  displayImage: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isProMember: {
+    type: Boolean,
+    default: false,
+  },
+  connectedDevices: {
+    type: Array,
+    default: [],
+  },
+  otp: {
+    type: String,
+    default: "",
+  },
+  otpExpiry: {
+    type: Date,
+    default: Date.now,
+  },
+  isEmailVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
