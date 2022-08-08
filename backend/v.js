acdd089efa78a6821deebabacb008fa04b1aac4c
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-08 01:15:16 +0100

    set up express server and crod and a whole lot of other stuff

--- backend/v.js
+++ backend/v.js
@@ -1,3 +1,23 @@
 const connect = require("./src/db/db");
-
 connect;
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const authRouter = require("./src/routes/auth");
+
+app.use("/auth", authRouter);
+
+const port = process.env.PORT || 1917;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
