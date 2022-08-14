31f965237a77739f62b98bdde16d9518a2f62c48
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-14 18:10:19 +0100

    added routes

--- backend/v.js
+++ backend/v.js
@@ -14,8 +14,14 @@ app.get("/", (req, res) => {
 });
 
 const authRouter = require("./src/routes/auth");
+const linkRouter = require("./src/routes/link");
+const boardRouter = require("./src/routes/board");
+const textRouter = require("./src/routes/text");
 
 app.use("/auth", authRouter);
+app.use("/link", linkRouter);
+app.use("/board", boardRouter);
+app.use("/text", textRouter);
 
 const port = process.env.PORT || 1917;
 app.listen(port, () => {
