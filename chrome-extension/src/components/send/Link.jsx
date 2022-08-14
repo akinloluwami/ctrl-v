cac94ccb41d7ab751e1309814bd0eff4bf97ba22
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-14 18:11:01 +0100

    added send link functionality (not working)

--- chrome-extension/src/components/send/Link.jsx
+++ chrome-extension/src/components/send/Link.jsx
@@ -1,13 +1,62 @@
 import { Box, Button, Input } from "@chakra-ui/react";
-import React, { Fragment } from "react";
+import React, { useState, Fragment, useRef } from "react";
+import { postData } from "../../../utils/useAxios";
 
 function SendLink() {
+  const [link, setLink] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const inputRef = useRef();
+
+  const handleSendLink = async () => {
+    setLoading(true);
+    const data = {
+      link,
+    };
+    try {
+      const res = await postData("/link", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          deviceToken: localStorage.getItem("deviceToken"),
+        },
+      });
+      if (res.status === 200) {
+        setSuccess(true);
+        setError(false);
+        setLink("");
+        inputRef.current.value = "";
+        console.log(res.data);
+        setLoading(false);
+      } else {
+        setLoading(false);
+        console.log(res.data.error);
+      }
+    } catch (err) {
+      setError(true);
+      setLoading(false);
+      setSuccess(false);
+    }
+  };
+
   return (
     <Fragment>
       <Box>
-        <Input type="text" placeholder="Paste link here" my={3} />
-        <Button backgroundColor={"#646cff"} color={"white"} width={"100%"}>
-          Send
+        <Input
+          type="text"
+          placeholder="Paste link here"
+          my={3}
+          onChange={(e) => setLink(e.target.value)}
+          ref={inputRef}
+        />
+        <Button
+          backgroundColor={"#646cff"}
+          color={"white"}
+          width={"100%"}
+          onClick={handleSendLink}
+          disabled={loading || link === ""}
+        >
+          {loading ? "Sending..." : "Send"}
         </Button>
       </Box>
     </Fragment>
