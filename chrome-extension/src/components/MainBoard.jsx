786542543cb62e4d6b9db1fc2f62b50559255bda
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-15 01:10:43 +0100

    cleared dummy code

--- chrome-extension/src/components/MainBoard.jsx
+++ chrome-extension/src/components/MainBoard.jsx
@@ -4,26 +4,11 @@ import React, { useEffect, useState } from "react";
 function MainBoard() {
   const [count, setCount] = useState(0);
 
-  //count seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount(count + 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [count]);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.reload();
-  };
-  return (
-    <>
-      <Text fontSize={"2em"} fontWeight={"bold"} textAlign={"center"}>
-        {count}
-      </Text>
-      <Button onClick={handleLogout}>Logout</Button>
-    </>
-  );
+  // const handleLogout = () => {
+  //   localStorage.removeItem("token");
+  //   window.location.reload();
+  // };
+  return <></>;
 }
 
 export default MainBoard;
