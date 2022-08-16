851e05a29a2f9eb5f2a64c63e9a400af1ee5c264
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-16 19:29:35 +0100

    Set up screen navigation

--- app/App.js
+++ app/App.js
@@ -1,13 +1,51 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import Onboarding from "./components/onboarding/Onboarding";
+import Home from "./screens/Home";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigator from "./navigation/Navigation";
+
+const Loading = () => {
+  <View>
+    <ActivityIndicator size="large" color="#0000ff" />
+  </View>;
+};
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [onboardingViewed, setOnboardingViewed] = useState(false);
+
+  const checkOnboarding = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@viewedOnboarding");
+      if (value !== null) {
+        setOnboardingViewed(true);
+      }
+    } catch (err) {
+      console.log("Error @checkOnboarding", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkOnboarding();
+  }),
+    [loading];
+
   return (
-    <View style={styles.container}>
-      <Onboarding />
+    <NavigationContainer>
+      {loading ? (
+        <Loading />
+      ) : onboardingViewed ? (
+        <Navigator />
+      ) : (
+        <Onboarding />
+      )}
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
