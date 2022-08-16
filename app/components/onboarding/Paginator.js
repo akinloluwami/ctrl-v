bc334e7c3820d73e8a0078a5d24993c6f1e3e7ff
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-16 11:29:43 +0100

    accepted scrollX prop

---
+++ app/components/onboarding/Paginator.js
@@ -0,0 +1,27 @@
+import { Text, StyleSheet, View } from "react-native";
+import React from "react";
+import colors from "../../utils/colors";
+
+export default Paginator = ({ data, scrollX }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        height: 64,
+      }}
+    >
+      {data.map((_, i) => {
+        return <View key={i.toString()} style={[styles.dot, { width: 12 }]} />;
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dot: {
+    height: 12,
+    borderRadius: 100,
+    margin: 8,
+    backgroundColor: colors.dots,
+  },
+});
