d173a6022f88d6aa8737c419eff095aa6b11e9bf
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-16 11:05:43 +0100

    populated the UI with the onboarding items

--- app/components/onboarding/Onboarding.js
+++ app/components/onboarding/Onboarding.js
@@ -1,10 +1,38 @@
-import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, Text, StyleSheet, FlatList, Animated } from "react-native";
 import onboardingData from "../../utils/onborading";
+import OnboardingItem from "./OnboardingItem";
+
 export default Onboarding = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const slidesRef = useRef(null);
+  const viewableItemsChanged = useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
   return (
     <View style={styles.container}>
-      <FlatList data={onboardingData} />
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={onboardingData}
+          renderItem={({ item }) => <OnboardingItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          bounces={false}
+          keyExtractor={(item) => onboardingData.indexOf(item).toString()}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: false }
+          )}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          scrollEventThrottle={32}
+          ref={slidesRef}
+        />
+      </View>
     </View>
   );
 };
