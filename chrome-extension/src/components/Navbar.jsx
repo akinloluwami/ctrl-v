5d59111e5681d6a38f641fc08f0f411ecc905d27
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-16 08:15:10 +0100

    fixed margin

--- chrome-extension/src/components/Navbar.jsx
+++ chrome-extension/src/components/Navbar.jsx
@@ -17,38 +17,35 @@ function Navbar({ selectedId, handleSelectedId }) {
     },
   ];
   return (
-    <>
-      <UnorderedList
-        listStyleType={"none"}
-        display={"flex"}
-        flexDirection={"row"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        padding="0.5em 0"
-        backgroundColor="rgba(0,0,0,0.2)"
-        width={"100%"}
-      >
-        {NavItems.map((item) => (
-          <ListItem key={item.name}>
-            <Button
-              color={selectedId === item.id ? "#fff" : "#646cff"}
-              background={
-                selectedId === item.id ? "#646cff" : "rgba(0,0,0,0.2)"
-              }
-              onClick={() => {
-                handleSelectedId(item.id);
-              }}
-              _hover={{
-                background: "#646cff",
-                color: "#fff",
-              }}
-            >
-              {item.name}
-            </Button>
-          </ListItem>
-        ))}
-      </UnorderedList>
-    </>
+    <UnorderedList
+      listStyleType={"none"}
+      display={"flex"}
+      flexDirection={"row"}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      padding="0.5em 0"
+      backgroundColor="rgba(0,0,0,0.2)"
+      width={"100%"}
+      margin={"0"}
+    >
+      {NavItems.map((item) => (
+        <ListItem key={item.name}>
+          <Button
+            color={selectedId === item.id ? "#fff" : "#646cff"}
+            background={selectedId === item.id ? "#646cff" : "rgba(0,0,0,0.2)"}
+            onClick={() => {
+              handleSelectedId(item.id);
+            }}
+            _hover={{
+              background: "#646cff",
+              color: "#fff",
+            }}
+          >
+            {item.name}
+          </Button>
+        </ListItem>
+      ))}
+    </UnorderedList>
   );
 }
 
