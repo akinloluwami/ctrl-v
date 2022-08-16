9be9d3daa1a1ea5e0dbb252c0e0b5a2efa37b99d
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-16 11:41:07 +0100

    made the width of the dots dynamic

--- app/components/onboarding/Paginator.js
+++ app/components/onboarding/Paginator.js
@@ -1,8 +1,9 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Animated, useWindowDimensions } from "react-native";
 import React from "react";
 import colors from "../../utils/colors";
 
 export default Paginator = ({ data, scrollX }) => {
+  const { width } = useWindowDimensions();
   return (
     <View
       style={{
@@ -11,7 +12,19 @@ export default Paginator = ({ data, scrollX }) => {
       }}
     >
       {data.map((_, i) => {
-        return <View key={i.toString()} style={[styles.dot, { width: 12 }]} />;
+        const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+        const dotWidth = scrollX.interpolate({
+          inputRange,
+          outputRange: [10, 20, 10],
+          extrapolate: "clamp",
+        });
+
+        return (
+          <Animated.View
+            key={i.toString()}
+            style={[styles.dot, { width: dotWidth }]}
+          />
+        );
       })}
     </View>
   );
@@ -19,7 +32,7 @@ export default Paginator = ({ data, scrollX }) => {
 
 const styles = StyleSheet.create({
   dot: {
-    height: 12,
+    height: 10,
     borderRadius: 100,
     margin: 8,
     backgroundColor: colors.dots,
