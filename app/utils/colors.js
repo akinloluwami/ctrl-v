8409ce2bb219ef6594eefcb3df15251c21cf59e7
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-18 15:55:29 +0100

    changed colors

--- app/utils/colors.js
+++ app/utils/colors.js
@@ -1,9 +1,10 @@
 const colors = {
-  accent: "#646cff",
-  accentRGB: "100, 108, 255",
-  text: "#aaa",
+  accent: "#0091FE",
+  accentRGB: "0, 145, 254",
+  text: "#fff",
   textRGB: "170, 170, 170",
-  dots: "rgba(100, 108, 255, 0.7)",
+  dots: "rgba(0, 145, 254, 0.7)",
+  background: "#171717",
 };
 
 export default colors;
