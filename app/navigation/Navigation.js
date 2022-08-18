5138112c0fa076cefa930ba24f708ee34d50d9cc
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-18 15:54:53 +0100

    Added authenticated check

--- app/navigation/Navigation.js
+++ app/navigation/Navigation.js
@@ -1,12 +1,44 @@
 import Home from "../screens/Home";
 import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Login from "../screens/Login";
+import { useEffect, useState } from "react";
+import Signup from "../screens/Signup";
 
 const Stack = createStackNavigator();
 
 export default function Navigator() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const checkAuth = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@auth");
+      if (value !== null) {
+        setIsAuthenticated(true);
+      }
+    } catch (err) {
+      console.log("Error @checkAuth", err);
+    } finally {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }),
+    [checkAuth];
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      {isAuthenticated ? (
+        <Stack.Screen name="Home" component={Home} />
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
+      <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
   );
 }
