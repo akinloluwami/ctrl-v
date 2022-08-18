1aa325b00da511ad52f9243aace6fb52ee97977e
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-18 15:53:36 +0100

    set up custom hook for axios

---
+++ app/utils/useAxios.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:1917";
+
+const postData = async (url, payload, config) => {
+  try {
+    const response = await axios.post(`${baseURL}${url}`, payload, config);
+    return response;
+  } catch (err) {
+    return err.response;
+  }
+};
+
+const getData = async (url, payload, config) => {
+  try {
+    const response = await axios.get(`${baseURL}${url}`, payload, config);
+    return response;
+  } catch (err) {
+    return err.response;
+  }
+};
+
+export { postData, getData };
