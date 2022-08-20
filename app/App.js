c330afda0ebf41283625a8dfeae4d1b869f65b09
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-20 23:54:04 +0100

    removed unused codes

--- app/App.js
+++ app/App.js
@@ -1,13 +1,8 @@
-import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
-import Onboarding from "./components/onboarding/Onboarding";
-import Home from "./screens/Home";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
-import Navigator from "./navigation/Navigation";
 import colors from "./utils/colors";
-
+import RootNavigator from "./navigation/RootNavigator";
 const Loading = () => {
   <View>
     <ActivityIndicator size="large" color="#0000ff" />
@@ -15,46 +10,10 @@ const Loading = () => {
 };
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [onboardingViewed, setOnboardingViewed] = useState(false);
-
-  const checkOnboarding = async () => {
-    try {
-      const value = await AsyncStorage.getItem("@viewedOnboarding");
-      if (value !== null) {
-        setOnboardingViewed(true);
-      }
-    } catch (err) {
-      console.log("Error @checkOnboarding", err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    checkOnboarding();
-  }),
-    [loading];
-
   return (
     <NavigationContainer theme={{ colors: { background: colors.background } }}>
-      {loading ? (
-        <Loading />
-      ) : onboardingViewed ? (
-        <Navigator />
-      ) : (
-        <Onboarding />
-      )}
+      <RootNavigator />
       <StatusBar backgroundColor="rgba(255,255,255,0.6)" />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
