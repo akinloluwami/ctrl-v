502f591af85e5e934e1265631c3c645c2e1e67af
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-20 23:55:36 +0100

    added root navigator

---
+++ app/navigation/RootNavigator.js
@@ -0,0 +1,62 @@
+import Home from "../screens/Home";
+import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import Signup from "../screens/auth/Signup";
+import Login from "../screens/auth/Login";
+import AuthNavigator from "./AuthNavigator";
+import BoardNavigator from "./BoardNavigator";
+import Onboarding from "../components/onboarding/Onboarding";
+
+const Root = createStackNavigator();
+
+export default function RootNavigator() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [onboardingViewed, setOnboardingViewed] = useState(false);
+
+  const checkOnboarding = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@viewedOnboarding");
+      if (value !== null) {
+        setOnboardingViewed(true);
+      }
+    } catch (err) {
+      console.log("Error @checkOnboarding", err);
+    }
+  };
+
+  useEffect(() => {
+    checkOnboarding();
+    console.log("onboardingViewed", onboardingViewed);
+  }),
+    [];
+  const checkAuth = async () => {
+    try {
+      const value = await AsyncStorage.getItem("token");
+      if (value !== null) {
+        setIsAuthenticated(true);
+      }
+    } catch (err) {
+      console.log("Error @checkAuth", err);
+    } finally {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }),
+    [checkAuth];
+
+  return (
+    <Root.Navigator screenOptions={{ headerShown: false }}>
+      {onboardingViewed ? (
+        <Root.Screen name="Auth" component={AuthNavigator} />
+      ) : isAuthenticated ? (
+        <Root.Screen name="Board" component={BoardNavigator} />
+      ) : (
+        <Root.Screen name="Onboarding" component={Onboarding} />
+      )}
+    </Root.Navigator>
+  );
+}
