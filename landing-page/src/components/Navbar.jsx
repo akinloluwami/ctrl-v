394494f8ff184367c83aa8ce742e9df88f5fff23
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-30 02:48:59 +0100

    fixed...

--- landing-page/src/components/Navbar.jsx
+++ landing-page/src/components/Navbar.jsx
@@ -4,28 +4,31 @@ import { RiMenu4Fill } from "react-icons/ri";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="navbar__logo">
-        <img src={logo} alt="" />
+    <div className="container">
+      <div className="navbar">
+        <div className="navbar__logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="navbar__links">
+          <li className="navbar__links__link">
+            <a href="#">Features</a>
+          </li>
+          <li className="navbar__links__link">
+            <a href="#">Platforms</a>
+          </li>
+          <li className="navbar__links__link">
+            <a href="#">Pricing</a>
+          </li>
+        </ul>
+        <div className="navbar__ctas">
+          <a href="#">Login</a>
+          <a href="#">Sign Up</a>
+        </div>
+        <div className="navbar__toggle">
+          <RiMenu4Fill className="io" />
+        </div>
       </div>
-      <ul className="navbar__links">
-        <li className="navbar__links__link">
-          <a href="#">Features</a>
-        </li>
-        <li className="navbar__links__link">
-          <a href="#">Platforms</a>
-        </li>
-        <li className="navbar__links__link">
-          <a href="#">Pricing</a>
-        </li>
-      </ul>
-      <div className="navbar__ctas">
-        <a href="#">Login</a>
-        <a href="#">Sign Up</a>
-      </div>
-      <div className="navbar__toggle">
-        <RiMenu4Fill className="io" />
-      </div>
+      <div className="navbar__mobile__menu"></div>
     </div>
   );
 }
