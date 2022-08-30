90e6c7314d9b7e56cf5704ebbeda969a7bb5020c
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-30 22:15:00 +0100

    removed unused code and imported platforms component

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./scss/App.scss";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features/Features";
+import Platforms from "./components/Platforms/Platforms";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <Features />
+      <Platforms />
     </div>
   );
 }
