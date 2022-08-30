1a8b4c2230668d022568e764619d33d312dd8b59
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-30 03:57:20 +0100

    added content

---
+++ landing-page/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>
+        Sync your <span>devices</span> with CtrlV
+      </h1>
+      <p>Share links, texts and files between your devives seamlessly.</p>
+      <center>
+        <a href="#">Get Started</a>
+      </center>
+    </div>
+  );
+}
+
+export default Hero;
