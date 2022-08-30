920d4032351622288cce8607ab68d6662b5ebef1
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-30 03:57:04 +0100

    imported hero component

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./scss/App.scss";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>CtrlV</h1>
+      <Hero />
     </div>
   );
 }
