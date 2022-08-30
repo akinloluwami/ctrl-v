cd5355c235ace41d66ab58a83bf640c87ea66b98
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-30 01:53:30 +0100

    added nav links

--- landing-page/src/components/Navbar.jsx
+++ landing-page/src/components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__logo"></div>
+      <ul className="navbar__links">
+        <li className="navbar__links__link">
+          <a href="#">Features</a>
+        </li>
+        <li className="navbar__links__link">
+          <a href="#">Platforms</a>
+        </li>
+        <li className="navbar__links__link">
+          <a href="#">Pricing</a>
+        </li>
+      </ul>
+      <div className="navbar__ctas"></div>
+    </div>
+  );
 }
 
 export default Navbar;
