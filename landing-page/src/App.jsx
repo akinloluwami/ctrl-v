df14930746bce8d05bc364e823d848414395deb8
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-31 20:31:11 +0100

    added pricing component

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features/Features";
 import Platforms from "./components/Platforms/Platforms";
+import Pricing from "./components/Pricing/Pricing";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Features />
       <Platforms />
+      <Pricing />
     </div>
   );
 }
