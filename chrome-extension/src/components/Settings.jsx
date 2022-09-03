68961ff5f3be487a1b4a9c004d92d8ac201a9d37
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-03 09:18:08 +0100

    added logout functionality

--- chrome-extension/src/components/Settings.jsx
+++ chrome-extension/src/components/Settings.jsx
@@ -1,5 +1,6 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 import React from "react";
+import { postData } from "../../utils/useAxios";
 
 function Settings() {
   return (
@@ -7,6 +8,24 @@ function Settings() {
       <Text fontSize={"2em"} fontWeight={"bold"} textAlign={"center"}>
         Settings
       </Text>
+      <Button
+        onClick={() => {
+          postData("/auth/logout", {
+            deviceToken: localStorage.getItem("deviceToken"),
+          })
+            .then((res) => {
+              localStorage.removeItem("token");
+              localStorage.removeItem("deviceToken");
+              console.log(res);
+              window.location.reload();
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }}
+      >
+        Logout
+      </Button>
     </>
   );
 }
