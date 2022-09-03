b231c410d4f2601b89f1401f9568ecb0a502f48e
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-03 09:16:30 +0100

    added a shit ton of things

--- chrome-extension/src/components/LinkDisplay.jsx
+++ chrome-extension/src/components/LinkDisplay.jsx
@@ -1,13 +1,17 @@
 import { Box, Flex, Button, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import moment from "moment";
+import { FaClone, FaCheck, FaClock } from "react-icons/fa";
 
 function LinkDisplay({ link, createdAt }) {
   const [copy, setCopy] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const truncate = (str) => {
     if (str.length > 30) {
       return str.slice(0, 30) + "...";
+    } else {
+      return str;
     }
   };
 
@@ -25,17 +29,48 @@ function LinkDisplay({ link, createdAt }) {
   };
 
   return (
-    <Flex justifyContent="space-between" alignItems="center">
+    <Flex
+      justifyContent="space-between"
+      alignItems="center"
+      my="15px"
+      backgroundColor={"rgba(0,0,0,0.2"}
+    >
       <Box>
-        <Text>{truncate(link)}</Text>
-        <Text>{moment(createdAt).fromNow()}</Text>
+        <Text fontWeight={"400"}>{truncate(link)}</Text>
+        <Text fontSize={"10px"} display="flex" alignItems={"center"} gap="5px">
+          <FaClock />
+          {moment(createdAt).fromNow()}
+        </Text>
       </Box>
       <Button
         onClick={() => {
           copyToClipboard(link);
         }}
+        background={"#646cff"}
+        _hover={{
+          backgroundColor: "#649cff",
+        }}
       >
-        Copy
+        <Box
+          transition="all ease 0.5s"
+          backgroundColor="transparent"
+          transform={`rotate(${copy ? "45deg" : "0"})}`}
+        >
+          {!copy ? (
+            <FaClone
+              style={{
+                backgroundColor: "transparent",
+              }}
+            />
+          ) : (
+            <FaCheck
+              style={{
+                backgroundColor: "transparent",
+                transform: "rotate(-45deg)",
+              }}
+            />
+          )}
+        </Box>
       </Button>
     </Flex>
   );
