67703d7bad99835cec2df399a8347facbc4b88b5
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-05 08:48:53 +0100

    changed shit man

--- app/api/RequestHandler.js
+++ app/api/RequestHandler.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://192.168.42.249:1917";
+const baseURL = "http://192.168.233.249:1917";
 
 const RequestHandler = axios.create({
   baseURL: baseURL,
