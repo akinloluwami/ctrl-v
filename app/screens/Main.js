45bb7a9711bc2c886caedc1e4ffb1933fe8cb8ee
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-06 22:54:02 +0100

    added main screen

---
+++ app/screens/Main.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigator from "../navigation/TabNavigator";
+
+const Main = () => {
+  return (
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default Main;
