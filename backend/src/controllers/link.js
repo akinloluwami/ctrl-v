2dcec1ddb5aba1af47741ecb9f2588ab3df806f6
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-06 22:55:09 +0100

    removed console log

--- backend/src/controllers/link.js
+++ backend/src/controllers/link.js
@@ -72,7 +72,6 @@ const getLinks = async (req, res) => {
     });
   }
   const links = await Link.find({ userId: _id });
-  // console.log(tkn + ".............." + deviceToken);
   res.status(200).json({
     links,
   });
