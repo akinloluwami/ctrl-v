f6297d6d437ea75d5ce1c02e667b5d704fefb075
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-14 23:30:11 +0100

    ...

--- chrome-extension/src/components/MainBoard.jsx
+++ chrome-extension/src/components/MainBoard.jsx
@@ -1,11 +1,13 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { getData, postData } from "../../utils/useAxios";
+import FileDisplay from "./FileDisplay";
 import LinkDisplay from "./LinkDisplay";
 import TextDisplay from "./TextDisplay";
 function MainBoard() {
   const [links, setLinks] = useState([]);
   const [texts, setTexts] = useState([]);
+  const [files, setFiles] = useState([]);
   const [data, setData] = useState([]);
 
   const getTexts = () => {
@@ -18,7 +20,6 @@ function MainBoard() {
       },
     }).then((res) => {
       setTexts(res.data.texts);
-      console.log(res);
     });
   };
 
@@ -32,19 +33,30 @@ function MainBoard() {
       },
     }).then((res) => {
       setLinks(res.data.links);
-      console.log(res);
+    });
+  };
+  const getFiles = () => {
+    getData("/file", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      params: {
+        deviceToken: localStorage.getItem("deviceToken"),
+      },
+    }).then((res) => {
+      setFiles(res.data.files);
     });
   };
 
   useEffect(() => {
     getLinks();
     getTexts();
+    getFiles();
   }, []);
 
   useEffect(() => {
-    setData([...links, ...texts]);
-    console.log(data);
-  }, [links, texts]);
+    setData([...links, ...texts, ...files]);
+  }, [links, texts, files]);
 
   return (
     <Box width="100%" margin={"auto"} marginTop="15px">
@@ -54,25 +66,22 @@ function MainBoard() {
             (a, b) =>
               new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
           )
-          .map((d, index) => {
-            if (d.type == "link") {
-              return (
-                <LinkDisplay
-                  key={index}
-                  link={d.link}
-                  createdAt={d.createdAt}
-                />
-              );
-            } else {
-              return (
-                <TextDisplay
-                  key={index}
-                  text={d.text}
-                  createdAt={d.createdAt}
-                />
-              );
-            }
-          })}
+          .map((d, index) =>
+            d.link ? (
+              <LinkDisplay key={index} link={d.link} createdAt={d.createdAt} />
+            ) : d.text ? (
+              <TextDisplay key={index} text={d.text} createdAt={d.createdAt} />
+            ) : (
+              <FileDisplay
+                key={index}
+                fileName={d.fileName}
+                createdAt={d.createdAt}
+                fileUrl={d.fileUrl}
+                fileFormat={d.fileFormat}
+                fileSize={d.fileSize}
+              />
+            )
+          )}
       </>
     </Box>
   );
