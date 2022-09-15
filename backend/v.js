4a50f7c2f9d1bfe74c7f91f4820ffff02142934c
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-15 23:19:59 +0100

    added express-fileupload middleware

--- backend/v.js
+++ backend/v.js
@@ -4,10 +4,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const fileupload = require("express-fileupload");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileupload({ useTempFiles: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -17,11 +19,13 @@ const authRouter = require("./src/routes/auth");
 const linkRouter = require("./src/routes/link");
 const boardRouter = require("./src/routes/board");
 const textRouter = require("./src/routes/text");
+const uploadRouter = require("./src/routes/upload");
 
 app.use("/auth", authRouter);
 app.use("/link", linkRouter);
 app.use("/board", boardRouter);
 app.use("/text", textRouter);
+app.use("/upload", uploadRouter);
 
 const port = process.env.PORT || 1917;
 app.listen(port, () => {
