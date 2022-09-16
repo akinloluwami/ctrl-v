8cdf17d51f77ff578376bdfd78587c5d8c3e23b4
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-16 23:25:38 +0100

    added file route

--- backend/v.js
+++ backend/v.js
@@ -25,7 +25,7 @@ app.use("/auth", authRouter);
 app.use("/link", linkRouter);
 app.use("/board", boardRouter);
 app.use("/text", textRouter);
-app.use("/upload", uploadRouter);
+app.use("/file", uploadRouter);
 
 const port = process.env.PORT || 1917;
 app.listen(port, () => {
