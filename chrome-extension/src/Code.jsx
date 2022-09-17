6cba295e0f07bfbe38f1c4eabf0e3476c5de6ef7
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-17 02:10:35 +0100

    .

---
+++ chrome-extension/src/Code.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Code = () => {
+  return (
+    <div>
+      <p></p>
+      <input />
+      <button>Click</button>
+    </div>
+  );
+};
+
+export default Code;
