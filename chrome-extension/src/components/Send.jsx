8d4c2bd1b12246236f94212108cfac29c8d590f0
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-18 15:39:45 +0100

    replaced conditional rendering with Chakra UI tab component

--- chrome-extension/src/components/Send.jsx
+++ chrome-extension/src/components/Send.jsx
@@ -1,4 +1,16 @@
-import { Box, Button, Flex, Link, Select, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Link,
+  Select,
+  Text,
+  Tab,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  TabList,
+} from "@chakra-ui/react";
 import React from "react";
 import { useState } from "react";
 import SendFile from "./send/File";
@@ -6,26 +18,6 @@ import SendText from "./send/Text";
 import SendLink from "./send/Link";
 
 function Send() {
-  const [option, setOption] = useState(0);
-
-  const options = [
-    {
-      id: 1,
-      name: "Link",
-      value: "link",
-    },
-    {
-      id: 2,
-      name: "Text",
-      value: "text",
-    },
-    {
-      id: 3,
-      name: "File",
-      value: "file",
-    },
-  ];
-
   return (
     <>
       <Text
@@ -36,27 +28,24 @@ function Send() {
       >
         What do you want to send?
       </Text>
-      <Flex justifyContent={"space-between"} my={"3"}>
-        {options.map((item) => (
-          <Button
-            key={item.id}
-            onClick={() => setOption(item.id)}
-            backgroundColor={option === item.id ? "#646cff" : "rgba(0,0,0,0.2)"}
-          >
-            {item.name}
-          </Button>
-        ))}
-      </Flex>
-      <Box>
-        {!option && (
-          <Text fontSize={"2em"} fontWeight={"bold"} textAlign={"center"}>
-            Select an option
-          </Text>
-        )}
-        {option === 1 && <SendLink />}
-        {option === 2 && <SendText />}
-        {option === 3 && <SendFile />}
-      </Box>
+      <Tabs variant="soft-rounded" colorScheme="purple" isFitted>
+        <TabList>
+          <Tab>Link</Tab>
+          <Tab>Text</Tab>
+          <Tab>File</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <SendLink />
+          </TabPanel>
+          <TabPanel>
+            <SendText />
+          </TabPanel>
+          <TabPanel>
+            <SendFile />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </>
   );
 }
