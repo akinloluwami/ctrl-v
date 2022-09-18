a4f4513c3f140d7eb40f16f96e9a9f64680f64b6
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-18 10:58:20 +0100

    removed unused imports

--- chrome-extension/src/layouts/BoardLayout.jsx
+++ chrome-extension/src/layouts/BoardLayout.jsx
@@ -10,10 +10,8 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { useCallback } from "react";
-import { useEffect } from "react";
 import { useState } from "react";
 import MainBoard from "../components/MainBoard";
-import Navbar from "../components/Navbar";
 import Send from "../components/Send";
 import Settings from "../components/Settings";
 
