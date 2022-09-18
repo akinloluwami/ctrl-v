1cd9200f7a956c8a9eed602918910284ff702f83
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-18 02:12:00 +0100

    .

--- landing-page/src/main.jsx
+++ landing-page/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Code from "./Code";
 import "./scss/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Code />
   </React.StrictMode>
 );
