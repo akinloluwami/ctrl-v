e58db634626568c3bed65d6785f55afd841d661a
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-18 02:11:54 +0100

    .

---
+++ landing-page/src/Code.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Code = () => {
+  setTimeout(() => {
+    console.log("hiii");
+  }, 2000);
+  return (
+    <div>
+      <input />
+      <input />
+      <input />
+
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default Code;
