ccd2604f8b980ecc939c256d8241650f324cbc74
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-19 11:35:57 +0100

    ...

--- app/api/RequestHandler.js
+++ app/api/RequestHandler.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://ctrlvapp.herokuapp.com";
+const baseURL = "http://192.168.102.249:1917";
 
 const RequestHandler = axios.create({
   baseURL: baseURL,
