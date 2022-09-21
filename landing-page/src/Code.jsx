3a76c831f361730f356d6a86bf64f02dfe9db5b3
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-21 22:44:17 +0100

    oh

--- landing-page/src/Code.jsx
+++ landing-page/src/Code.jsx
@@ -1,16 +1,58 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 const Code = () => {
-  setTimeout(() => {
-    console.log("hiii");
-  }, 2000);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [message, setMessage] = useState("");
+  const [otp, setOtp] = useState("");
+
+  function verifyOTP(e) {
+    const OTP = "6h5s3n";
+    e.preventDefault();
+    if (OTP === otp) {
+      setError(false);
+      setSuccess(true);
+      setMessage("Email has been verified");
+    } else {
+      setSuccess(false);
+      setError(true);
+      setMessage("Invalid OTP");
+    }
+  }
+
   return (
     <div>
-      <input />
-      <input />
-      <input />
+      <h1>Verify OTP</h1>
 
-      <button>Submit</button>
+      {error && (
+        <h1
+          style={{
+            color: "red",
+          }}
+        >
+          {message}
+        </h1>
+      )}
+      {success && (
+        <h1
+          style={{
+            color: "green",
+          }}
+        >
+          {message}
+        </h1>
+      )}
+
+      <form onSubmit={verifyOTP}>
+        <input
+          type="text"
+          onChange={(e) => {
+            setOtp(e.target.value);
+          }}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
