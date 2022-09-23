36272c148c2b11d931b1471809e831e6c50f7ede
Timileyin <timileyinoyelekan11@gmail.com>
2022-09-23 20:20:36 +0100

    fix: hero section spacing and padding

--- landing-page/src/components/Hero.jsx
+++ landing-page/src/components/Hero.jsx
@@ -9,8 +9,6 @@ function Hero() {
       <p>Share links, texts and files between your devices seamlessly.</p>
       <center>
         <a href="/">Download</a>
-        <br />
-        <br />
         <small>It's free, seriously</small>
       </center>
     </div>
