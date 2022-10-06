22cbb9a3d7102d2cb4747b0d89b97483d7a99591
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-06 04:22:31 +0100

    refactor

--- app/utils/useAxios.js
+++ app/utils/useAxios.js
@@ -1,23 +1,22 @@
 import axios from "axios";
 
-const baseURL = "http://172.23.240.1:1917";
+const baseURL = "http://192.168.124.249:1917";
 
-const postData = async (url, payload, config) => {
+const postData = async (route, payload) => {
   try {
-    const response = await axios.post(`${baseURL}${url}`, payload, config);
+    const response = await axios.post(`${baseURL}${route}`, payload);
     return response;
   } catch (err) {
     return err.response;
   }
 };
 
-const getData = async (url, payload, config) => {
+const getData = async (route, payload, config) => {
   try {
-    const response = await axios.get(`${baseURL}${url}`, payload, config);
+    const response = await axios.get(`${baseURL}${route}`, payload, config);
     return response;
   } catch (err) {
     return err.response;
   }
 };
-
 export { postData, getData };
